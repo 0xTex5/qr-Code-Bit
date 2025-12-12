9a3966c67e2e233b9c55f39e7ebf1c883a6ade6a
Github-coder-plus <181423861+Github-coder-plus@users.noreply.github.com>
2025-12-12 10:36:18 -0700

    initial commit

--- microbitfrontend.ts
+++ microbitfrontend.ts
@@ -1,1 +1,10 @@
 // hey im adding thsi comment for funsies
+// start of program
+led.plot(4, 4)
+let x = 0
+let y = 0
+basic.forever(function () {
+    input.onButtonPressed(Button.A, function() {
+        led.toggle(x+1,y)
+    })
+})
